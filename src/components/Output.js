f54586f85c0b42a5b46f968c28f7f546e5a752a1
rayGreycloud <rayGreycloud@gmail.com>
2017-08-23 14:00:03 -0700

    Add well to output

--- src/components/Output.js
+++ src/components/Output.js
@@ -11,7 +11,7 @@ class Output extends Component {
 
   render() {
     return (
-      <div className="output">
+      <div className="well output">
         {this.props.value}
       </div>
     );
