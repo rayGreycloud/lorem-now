392a9e8c3f24f76d567a56ce149c1a2eefb4a9e4
rayGreycloud <rayGreycloud@gmail.com>
2017-08-23 09:03:10 -0700

    Change lorem api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      paragraphs: 3,
+      format: 'text',
+      text: ''
+    }
+  }
+
+  componentWillMount() {
+    this.getLorem();
+  }
+
+  getLorem() {
+    const { paragraphs, format } = this.state;
+    const baseUrl = 'https://dinoipsum.herokuapp.com/api';
+
+    axios.get(`${baseUrl}?format=${format}&paragraphs=${paragraphs}`)
+      .then((response) => {
+        this.setState({
+          text: response.data
+        }, function() {
+          console.log(this.state);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <div className="App">
